3aa7fb42b4d3161abe36e66b22d3ec19d5508e25
Justin Ha <jch583@nyu.edu>
2016-10-27 16:24:06 -0400

    Procedural Generation works

--- Assets/Scripts/Pathmaker.cs
+++ Assets/Scripts/Pathmaker.cs
@@ -7,13 +7,22 @@ public class Pathmaker : MonoBehaviour {
 	public Transform floorPrefab;
 	public Transform pathmakerSpherePrefab; 
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
 	// Update is called once per frame
 	void Update () {
-	
+		if (counter < 50) {
+			float randomNumber = Random.value;
+			if (randomNumber < 0.25f) {
+				transform.Rotate (0f, 90f, 0f);
+			} else if (randomNumber > 0.25f && randomNumber < 0.5f) {
+				transform.Rotate (0, -90f, 0f);
+			} else if (randomNumber > 0.99f && randomNumber < 1.0f) {
+				Instantiate (pathmakerSpherePrefab, transform.position, transform.rotation);
+			}
+			Instantiate (floorPrefab, transform.position, transform.rotation);
+			transform.Translate (0f, 0f, 5f);
+			counter += 1;
+		} else {
+			Destroy (gameObject);
+		}
 	}
 }
