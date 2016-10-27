8f3002501d76412ee4eb6f4403f5f3d4750faa6c
Justin Ha <jch583@nyu.edu>
2016-10-27 15:51:20 -0400

    Started Procedural Generation Lab

---
+++ Assets/Scripts/Pathmaker.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pathmaker : MonoBehaviour {
+
+	private int counter = 0;
+	public Transform floorPrefab;
+	public Transform pathmakerSpherePrefab; 
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
