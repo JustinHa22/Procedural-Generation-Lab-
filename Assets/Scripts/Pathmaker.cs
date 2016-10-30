8e0275b32c76fe14499b4671ba472747f998a41a
Justin Ha <jch583@nyu.edu>
2016-10-30 17:03:08 -0400

    Changed camera view

--- Assets/Scripts/Pathmaker.cs
+++ Assets/Scripts/Pathmaker.cs
@@ -3,24 +3,25 @@ using System.Collections;
 
 public class Pathmaker : MonoBehaviour {
 
-	private int counter = 0;
 	public Transform floorPrefab;
 	public Transform pathmakerSpherePrefab; 
+	public static int numberOfTiles; 
 
 	// Update is called once per frame
 	void Update () {
-		if (counter < 50) {
+		if ( numberOfTiles < 500) {
 			float randomNumber = Random.value;
 			if (randomNumber < 0.25f) {
 				transform.Rotate (0f, 90f, 0f);
 			} else if (randomNumber > 0.25f && randomNumber < 0.5f) {
 				transform.Rotate (0, -90f, 0f);
-			} else if (randomNumber > 0.99f && randomNumber < 1.0f) {
+			} else if (randomNumber > 0.95f) {
 				Instantiate (pathmakerSpherePrefab, transform.position, transform.rotation);
 			}
 			Instantiate (floorPrefab, transform.position, transform.rotation);
 			transform.Translate (0f, 0f, 5f);
-			counter += 1;
+			//counter += 1;
+			numberOfTiles += 1; 
 		} else {
 			Destroy (gameObject);
 		}
