7eb9600a0ec745fa3cb203124d7994307ac8a41a
Justin Ha <jch583@nyu.edu>
2016-10-31 18:57:47 -0400

    Messed around with PathMaker lifetimes through tiles

--- Assets/Scripts/Pathmaker.cs
+++ Assets/Scripts/Pathmaker.cs
@@ -5,7 +5,15 @@ public class Pathmaker : MonoBehaviour {
 
 	public Transform floorPrefab;
 	public Transform pathmakerSpherePrefab; 
+	public int pathmakerLifeTime = 0; 
+	public int tilesPerPathmaker; 
 	public static int numberOfTiles; 
+
+	public static int numberOfPathmakers = 0;
+
+	void Start(){
+		tilesPerPathmaker = Random.Range(35, 50);
+	}
 
 	// Update is called once per frame
 	void Update () {
@@ -15,15 +23,22 @@ public class Pathmaker : MonoBehaviour {
 				transform.Rotate (0f, 90f, 0f);
 			} else if (randomNumber > 0.25f && randomNumber < 0.5f) {
 				transform.Rotate (0, -90f, 0f);
-			} else if (randomNumber > 0.95f) {
+			} else if (randomNumber > 0.70f && numberOfPathmakers < 10) {
 				Instantiate (pathmakerSpherePrefab, transform.position, transform.rotation);
+				numberOfPathmakers += 1; 
 			}
 			Instantiate (floorPrefab, transform.position, transform.rotation);
 			transform.Translate (0f, 0f, 5f);
 			//counter += 1;
 			numberOfTiles += 1; 
-		} else {
+			pathmakerLifeTime += 1;
+		} 
+
+		if (pathmakerLifeTime == tilesPerPathmaker || numberOfTiles > 500){
+			numberOfPathmakers -= 1;
+			Debug.Log (numberOfTiles);
 			Destroy (gameObject);
+			Debug.Log ("working");
 		}
 	}
 }
